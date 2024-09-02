5ec8bc8542c4832b2615450b68cbe58eb7e10071
sanilshrmiu <129837713+sanilshrmiu@users.noreply.github.com>
2024-09-02 00:03:56 -0400

    first commit

---
+++ src/main/java/com/miu/cs/cs425/demowebapps/sanilelibrary/ElibraryApplication.java
@@ -0,0 +1,13 @@
+package com.miu.cs.cs425.demowebapps.sanilelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miu/cs/cs425/demowebapps/sanilelibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.cs.cs425.demowebapps.sanilelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
